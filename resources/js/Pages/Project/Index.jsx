b79e4f1ccbc6e14f7568baa53456709f91cb0b7f
bibekbhandarig67 <bibekbhandarig61@gmail.com>
2024-08-21 13:51:14 +0545

    project delete

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -39,6 +39,15 @@ export default function Index({ auth, projects, queryParams = null, success }) {
     router.get(route("project.index"), queryParams);
   };
 
+  const deleteProject = (project) => {
+    if(!window.confirm('Are you sure you want to delete this project?')) {
+      return;
+    }
+
+    router.delete(route('project.destroy', project.id));
+    // href={route("project.destroy", project.id)}
+  };
+
   return (
     <AuthenticatedLayout
       user={auth.user}
@@ -183,19 +192,19 @@ export default function Index({ auth, projects, queryParams = null, success }) {
                         <td className="px-3 py-3">{project.created_at}</td>
                         <td className="px-3 py-3">{project.due_date}</td>
                         <td className="px-3 py-3">{project.createdBy.name}</td>
-                        <td className="px-3 py-3">
+                        <td className="px-3 py-3 text-nowrap">
                           <Link
                             href={route("project.edit", project.id)}
                             className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1"
                           >
                             Edit
                           </Link>
-                          <Link
-                            href={route("project.edit", project.id)}
+                          <button
+                            onClick={e => deleteProject(project)}
                             className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"
                           >
                             Delete
-                          </Link>
+                          </button>
                         </td>
                       </tr>
                     ))}
