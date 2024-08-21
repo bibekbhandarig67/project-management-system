dad4a633f54a39133412936671b39c7a445fe9c0
bibekbhandarig67 <bibekbhandarig61@gmail.com>
2024-08-21 14:50:55 +0545

    project edit

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\UpdateProjectRequest;
 use App\Http\Resources\ProjectResource;
 use App\Http\Resources\TaskResource;
 use Illuminate\Support\Facades\Auth;
+use Storage;
 use Str;
 // use App\Http\Controllers\Inertia;
 
@@ -101,7 +102,9 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
-        //
+        return inertia('Project/Edit', [
+            'project' => new ProjectResource($project),
+        ]);
     }
 
     /**
@@ -109,7 +112,20 @@ class ProjectController extends Controller
      */
     public function update(UpdateProjectRequest $request, Project $project)
     {
-        //
+        $data = $request->validated();
+
+        $image = $data['image'] ?? null;
+        $data['updated_by'] = Auth::id();
+
+        if ($image) {
+            if($project->image_path) {
+                Storage::disk('public')->deleteDirectory(dirname($project->image_path));
+            }
+            $data['image_path'] = $image->store('project/' . Str::random(), 'public');
+        }
+        
+        $project->update($data);
+        return to_route('project.index')->with('success',"Project \"$project->name\" was updated");
     }
 
     /**
@@ -119,6 +135,9 @@ class ProjectController extends Controller
     {
         $name = $project->name;
         $project->delete();
+        if($project->image_path) {
+            Storage::disk('public')->deleteDirectory(dirname($project->image_path));
+        }
         return to_route('project.index')->with('success', "Project \"$name\" deleted successfully");
     }
 }
